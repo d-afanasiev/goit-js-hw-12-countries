e75a1d496342db5a5209db02ffe00f2c4653a7e0
Afanasiev Dmitriy <59284294+d-afanasiev@users.noreply.github.com>
2021-06-16 16:36:18 +0300

    fix function inputCountries, add message, add hbs

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,18 @@
-import fetchCountries from './fetchCountries.js';
 const debounce = require('lodash.debounce');
+
+import { error } from '@pnotify/core';
+import * as PNotifyAnimate from '@pnotify/animate';
+import { defaults } from '@pnotify/animate';
+const { defaults: coreDefaults } = require('@pnotify/core');
+coreDefaults.width = '400px';
+defaults.inClass = 'fadeInDown';
+defaults.outClass = 'fadeOutUp';
+import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/PNotify.css';
+
+import cardsTemplate from '../partials/cards.hbs';
+import cardTemplate from '../partials/card.hbs';
+import fetchCountries from './fetchCountries.js';
 
 const refs = {
   inputCountries: document.querySelector('.js-input'),
@@ -10,15 +23,26 @@ refs.inputCountries.addEventListener('input', debounce(inputCountries, 500));
 
 function inputCountries(e) {
   refs.insertList.innerHTML = '';
-  fetchCountries(e.target.value).then(countries => {
-    console.log(countries.length);
-    if (countries.length >= 2 && countries.length <= 10) {
-      const list = countries
-        .map(countrie => {
-          return `<li>${countrie.name}</li>`;
-        })
-        .join('');
-      refs.insertList.innerHTML = `<ul class="js-country">${list}</ul>`;
-    }
+  fetchCountries(e.target.value.trim())
+    .then(countries => {
+      if (e.target.value.trim() === '') {
+        return;
+      }
+      if (countries.length === 1) {
+        refs.insertList.innerHTML = cardTemplate(countries);
+      } else if (countries.length >= 2 && countries.length <= 10) {
+        refs.insertList.innerHTML = cardsTemplate(countries);
+      } else if (countries.length > 10) {
+        errorMessage('Too many matches found. Please enter a more specific query!');
+      } else if (GET === 404) {
+        errorMessage('No country has been found. Please enter a more specific query!');
+      }
+    })
+    .catch(() => errorMessage('No country has been found. Please enter a more specific query!'));
+}
+
+function errorMessage(message) {
+  error({
+    text: `${message}`,
   });
 }
